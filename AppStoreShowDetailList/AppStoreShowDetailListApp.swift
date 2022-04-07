9a4b1ed7b04b14d2b9410c18c7fbc9df884ed133
张亚飞 <672233580@qq.com>
2022-04-07 14:08:36 +0800

    Initial Commit

---
+++ AppStoreShowDetailList/AppStoreShowDetailListApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreShowDetailListApp.swift
+//  AppStoreShowDetailList
+//
+//  Created by 张亚飞 on 2022/4/7.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreShowDetailListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppStoreShowDetailList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppStoreShowDetailList
+//
+//  Created by 张亚飞 on 2022/4/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
